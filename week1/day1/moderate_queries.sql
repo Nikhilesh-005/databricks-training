40972c45f39c325ef274e869c9a00bc7bb12ffc4
NIKHILESH ALLADASETTI <155538545+Nikhilesh-005@users.noreply.github.com>
2026-05-06 15:56:06 +0530

    updated Moderate queries section

--- week1/day1/moderate_queries.sql
+++ week1/day1/moderate_queries.sql
@@ -1,74 +1,55 @@
--- Combined Moderate Difficulty Queries
-
 -- Q56
-SELECT SUM(salary)
-FROM Employee
-WHERE YEAR(hire_date) = 2020;
+select sum(salary) from Employee
+where year(hire_date)=2020;
 
--- Q57
-SELECT department_id, AVG(salary) AS avg_salary
-FROM Employee
+--Q57
+SELECT department_id,avg(salary) as avg_salary from Employee
 GROUP BY department_id
-ORDER BY avg_salary DESC;
+order by avg_salary desc;
 
--- Q58
-SELECT department_id
-FROM Employee
-GROUP BY department_id
-HAVING COUNT(*) > 1 AND AVG(salary) > 55000;
+-- q58
+select department_id from Employee group by department_id
+having count(*)>1 and avg(salary)>55000;
 
--- Q59
-SELECT *
-FROM Employee
-WHERE hire_date >= CURDATE() - INTERVAL 2 YEAR
-ORDER BY hire_date;
+--Q59
+select * from Employee
+where hire_date >= curdate()-interval 2 year
+order by hire_date;
 
--- Q60
-SELECT department_id, COUNT(*) AS total_employees,
-AVG(salary) AS avg_salary
-FROM Employee
-GROUP BY department_id
-HAVING COUNT(*) > 2;
+--Q60
+SELECT department_id,count(*) as total_employees,
+avg(salary) as avg_salary from Employee
+group by department_id
+having count(*)>2;
 
--- Q61
-SELECT name, salary
-FROM Employee e
-WHERE salary > (
-    SELECT AVG(salary)
-    FROM Employee
-    WHERE department_id = e.department_id
+--Q61
+select name,salary from Employee e
+where salary>( select avg(salary)
+from Employee where department_id=e.department_id
 );
 
--- Q62
-SELECT name
-FROM Employee
-WHERE hire_date = (
-    SELECT MIN(hire_date)
-    FROM Employee
+--Q62
+select name from Employee
+where hire_date=( select min(hire_date)
+from Employee
 );
 
--- Q63
-SELECT d.name, COUNT(p.project_id) AS total_projects
-FROM Department d
-JOIN Project p
-ON d.department_id = p.department_id
-GROUP BY d.name
-ORDER BY total_projects;
+--Q63
+select d.name,count(p.project_id) as total_projects
+from Department d join Project p
+on d.department_id=p.department_id
+group by d.name
+order by total_projects;
 
--- Q64
-SELECT e.name, e.salary, e.department_id
-FROM Employee e
-WHERE salary = (
-    SELECT MAX(salary)
-    FROM Employee
-    WHERE department_id = e.department_id
+--Q64
+select e.name,e.salary,e.department_id
+from Employee e
+where salary=(select max(salary)
+from Employee where department_id=e.department_id
 );
 
--- Q65
-SELECT name, salary
-FROM Employee e
-WHERE age > (
-    SELECT AVG(age)
-    FROM Employee
-    WHERE department_id = e.department_id
+--Q65
+select name,salary from Employee e
+where age>(select avg(age) from Employee
+where department_id=e.department_id
 );
